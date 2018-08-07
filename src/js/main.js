e84b958b57ffbec96529ce5fb6c23e603002c2c1
K00ry <Koory.yellow@gmail.com>
2018-08-07 01:17:28 -0400

    Added Segment

--- src/js/main.js
+++ src/js/main.js
@@ -6,11 +6,73 @@ import ScrollTrigger from 'scrolltrigger-classes';
 import { skills } from '../data';
 
 export default function main() {
+  /////////////// SEGMENT \\\\\\\\\\\\\\\\\\\\
+
+  !(function() {
+    var analytics = (window.analytics = window.analytics || []);
+    if (!analytics.initialize)
+      if (analytics.invoked)
+        window.console &&
+          console.error &&
+          console.error('Segment snippet included twice.');
+      else {
+        analytics.invoked = !0;
+        analytics.methods = [
+          'trackSubmit',
+          'trackClick',
+          'trackLink',
+          'trackForm',
+          'pageview',
+          'identify',
+          'reset',
+          'group',
+          'track',
+          'ready',
+          'alias',
+          'debug',
+          'page',
+          'once',
+          'off',
+          'on',
+        ];
+        analytics.factory = function(t) {
+          return function() {
+            var e = Array.prototype.slice.call(arguments);
+            e.unshift(t);
+            analytics.push(e);
+            return analytics;
+          };
+        };
+        for (var t = 0; t < analytics.methods.length; t++) {
+          var e = analytics.methods[t];
+          analytics[e] = analytics.factory(e);
+        }
+        analytics.load = function(t, e) {
+          var n = document.createElement('script');
+          n.type = 'text/javascript';
+          n.async = !0;
+          n.src =
+            'https://cdn.segment.com/analytics.js/v1/' +
+            t +
+            '/analytics.min.js';
+          var a = document.getElementsByTagName('script')[0];
+          a.parentNode.insertBefore(n, a);
+          analytics._loadOptions = e;
+        };
+        analytics.SNIPPET_VERSION = '4.1.0';
+        analytics.load('h93L2A5itVQJTKOGwmIMz7pTstEbyXgy');
+        analytics.page();
+      }
+  })();
+
+  /////////////////////   scroll triiger   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
   document.addEventListener('DOMContentLoaded', function() {
     var trigger = new ScrollTrigger({
       centerVertical: true,
     });
   });
+
+  ///////////////////  hight lighting the area of exploring \\\\\\\\\\\\\\
 
   let controller = new ScrollMagic.Controller({
     globalSceneOptions: { triggerHook: 0.1, duration: '200' },
